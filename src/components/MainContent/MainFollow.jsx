790be20a7007665609ebffcca448910ce05da15c
leeyeonji28 <yeon102800@gmail.com>
2022-09-14 01:15:43 +0900

    main follow

--- src/components/MainContent/MainFollow.jsx
+++ src/components/MainContent/MainFollow.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import proimg from "../../assets/images/ding.png";
 
 const MainFollow = () => {
-  return <div>MainFollow</div>;
+  // 데모 데이터
+  const followData = [
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+    {
+      userId: 2,
+      userImg: proimg,
+    },
+  ];
+  return (
+    <div className="p-7">
+      <h3 className="mb-2">
+        <b className="text-xl mr-2">Follow</b>
+        <span className="text-gray-400">{followData.length}</span>
+      </h3>
+      <ul className="flex flex-wrap justify-around">
+        {followData.map((follow, i) => (
+          <li key={i} className="w-16 shadow-md mb-2 rounded-lg">
+            <img src={follow.userImg} alt="" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MainFollow;
