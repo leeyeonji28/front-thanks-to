84cdf1db47f6550413d7a5ca275421c15c901576
leeyeonji28 <yeon102800@gmail.com>
2022-09-22 09:58:13 +0900

    Join 이미지 첨부 시 미리보기 추가

--- src/routes/Join.jsx
+++ src/routes/Join.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiOutlineCamera } from "react-icons/hi";
 
 const Join = () => {
+  const [imageSrc, setImageSrc] = useState();
+  const [files, setFiles] = useState([]);
+
+  const encodeFileToBase64 = (fileBlob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result);
+        resolve();
+      };
+    });
+  };
+
+  const previewPostImg = (e) => {
+    encodeFileToBase64(e.target.files[0]);
+  };
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    setFiles([...files, { uploadedFile: file }]);
+  };
+
   return (
     <div className="flex justify-center items-center w-full h-full">
       <div className="py-12 px-28 text-center bg-white rounded-lg">
@@ -24,10 +48,10 @@ const Join = () => {
             placeholder="Nick Name"
             className="block w-[350px] p-4 mb-4 rounded-lg border border-gray-300 outline-rose-500"
           />
-          <div>
+          <div className="relative">
             <label
               htmlFor="selector_img"
-              className="flex justify-center items-center w-[350px] h-32 mb-4 rounded-lg bg-gray-300 cursor-pointer"
+              className="flex justify-center items-center w-[350px] h-52 mb-4 rounded-lg bg-gray-300 cursor-pointer"
             >
               <div>
                 <HiOutlineCamera className="m-auto text-5xl text-white " />
@@ -40,12 +64,25 @@ const Join = () => {
               id="selector_img"
               encType="multipart/form-data"
               multiple="multiple"
-              // onChange={(e) => {
-              //   previewPostImg(e);
-              //   handleUpload(e);
-              // }}
+              onChange={(e) => {
+                previewPostImg(e);
+                handleUpload(e);
+              }}
               className="hidden"
             />
+            <div
+              className={
+                imageSrc
+                  ? "absolute top-0 w-[350px] h-52 rounded-lg bg-white overflow-hidden"
+                  : "hidden"
+              }
+            >
+              <img
+                src={imageSrc}
+                alt="preview-img"
+                className="absolute top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 w-[350px]"
+              />
+            </div>
           </div>
           <textarea
             placeholder="Nick Name"
