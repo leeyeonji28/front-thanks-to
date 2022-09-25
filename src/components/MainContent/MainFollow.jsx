f299c2959eae90ec8150650cee76113343eb4321
leeyeonji28 <yeon102800@gmail.com>
2022-09-25 23:20:44 +0900

    follow html 수정

--- src/components/MainContent/MainFollow.jsx
+++ src/components/MainContent/MainFollow.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import proimg from "../../assets/images/ding.png";
+import { AiOutlineDoubleRight } from "react-icons/ai";
 
 const MainFollow = () => {
   // 데모 데이터
@@ -47,9 +48,15 @@ const MainFollow = () => {
   ];
   return (
     <div className="p-7">
-      <h3 className="mb-5">
-        <b className="text-xl mr-2">Follow</b>
-        <span className="text-gray-400">{followData.length}</span>
+      <h3 className="flex justify-between items-center mb-5">
+        <div>
+          <b className="text-xl mr-2">Follow</b>
+          <span className="text-gray-400">{followData.length}</span>
+        </div>
+        <p className="inline-block ml-5 text-gray-400 cursor-pointer">
+          전체보기
+          <AiOutlineDoubleRight className="inline-block" />
+        </p>
       </h3>
       <ul className="flex flex-wrap justify-around">
         {followData.map((follow, i) => (
