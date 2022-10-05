885c63fe8836c2393da8feda7da56496b156f43a
leeyeonji28 <yeon102800@gmail.com>
2022-10-05 17:40:21 +0900

    post masonry 레이아웃 수정

--- src/components/PostContent/PostBox.jsx
+++ src/components/PostContent/PostBox.jsx
@@ -2,17 +2,24 @@ import React from "react";
 
 const PostBox = ({ postImg, postTitle, postContent, postDate, userName }) => {
   return (
-    <div
-      className="w-[376px] rounded-lg bg-white
-    mb-6"
-    >
-      <div>
-        <img src={postImg} alt="" />
+    <div className="w-full rounded-lg bg-white mb-6">
+      {postImg != "" ? (
+        <div className="flex justify-center items-center h-72 rounded-t-lg overflow-hidden">
+          <img src={postImg} alt="" />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="p-3">
+        <b className="block mb-3 text-xl">{postTitle}</b>
+        <p>
+          {postContent.length > 200
+            ? `${postContent.slice(0, 200)}...`
+            : postContent}
+        </p>
+        <p className="mt-3 text-gray-400">{postDate}</p>
       </div>
-      <b>{postTitle}</b>
-      <p>{postContent}</p>
-      <p>{postDate}</p>
-      <div>user 정보</div>
+      <div className="p-3 border-t">user 정보</div>
     </div>
   );
 };
--- src/routes/Post.jsx
+++ src/routes/Post.jsx
@@ -6,6 +6,7 @@ import { CgSpinner } from "react-icons/cg";
 import axios from "axios";
 import { url } from "../utile/url";
 import PostBox from "../components/PostContent/PostBox";
+import Masonry from "react-masonry-css";
 
 const Post = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -55,23 +56,31 @@ const Post = () => {
       </div>
     );
   }
+  const columnsObs = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
   return (
     <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[1270px] h-[925px] m-auto">
       <Header />
-      <div className="flex ">
+      <div className="flex">
         <SideNav />
-        <div className="w-[1170px] h-[855px] rounded-lg text-center overflow-scroll">
-          <div className="grid grid-cols-3">
-            {getPostData.map((data, index) => (
-              <PostBox
-                key={index}
-                postImg={data.postImg}
-                postTitle={data.postTitle}
-                postContent={data.postContent}
-                postDate={data.postDate}
-                // userName={data.user.nickName}
-              />
-            ))}
+        <div className="w-[1170px] h-[855px] rounded-lg overflow-y-scroll scrollbar-hide">
+          <div>
+            <Masonry breakpointCols={3} className="flex gap-5">
+              {getPostData.map((data, index) => (
+                <PostBox
+                  key={index}
+                  postImg={data.postImg}
+                  postTitle={data.postTitle}
+                  postContent={data.postContent}
+                  postDate={data.postDate}
+                  // userName={data.user.nickName}
+                />
+              ))}
+            </Masonry>
           </div>
         </div>
       </div>
